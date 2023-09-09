200b40d52de11d06786b4866e730ac5b03f0e281
aguercoder <aguer.coder@gmail.com>
2023-09-09 21:54:53 +0200

    Example use of dto generator

---
+++ example-dto-generator/src/main/java/aguerchi/dto/generator/SampleEntity.java
@@ -0,0 +1,100 @@
+package aguerchi.dto.generator;
+
+import com.dto.generator.quarkus.runtime.annotation.DtoGenerator;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+@DtoGenerator
+public class SampleEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int intField;
+    private double doubleField;
+    private float floatField;
+    private long longField;
+    private short shortField;
+    private byte byteField;
+    private char charField;
+    private boolean booleanField;
+
+    // Getter and Setter methods for each field
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getIntField() {
+        return intField;
+    }
+
+    public void setIntField(int intField) {
+        this.intField = intField;
+    }
+
+    public double getDoubleField() {
+        return doubleField;
+    }
+
+    public void setDoubleField(double doubleField) {
+        this.doubleField = doubleField;
+    }
+
+    public float getFloatField() {
+        return floatField;
+    }
+
+    public void setFloatField(float floatField) {
+        this.floatField = floatField;
+    }
+
+    public long getLongField() {
+        return longField;
+    }
+
+    public void setLongField(long longField) {
+        this.longField = longField;
+    }
+
+    public short getShortField() {
+        return shortField;
+    }
+
+    public void setShortField(short shortField) {
+        this.shortField = shortField;
+    }
+
+    public byte getByteField() {
+        return byteField;
+    }
+
+    public void setByteField(byte byteField) {
+        this.byteField = byteField;
+    }
+
+    public char getCharField() {
+        return charField;
+    }
+
+    public void setCharField(char charField) {
+        this.charField = charField;
+    }
+
+    public boolean isBooleanField() {
+        return booleanField;
+    }
+
+    public void setBooleanField(boolean booleanField) {
+        this.booleanField = booleanField;
+    }
+}
---
+++ example-dto-generator/src/main/java/aguerchi/dto/generator/data/request/SampleEntityRequestDTO.java
@@ -0,0 +1,69 @@
+package aguerchi.dto.generator.data.request;
+import java.lang.Long;
+
+public class SampleEntityRequestDTO {
+    private boolean booleanField;
+    private byte byteField;
+    private char charField;
+    private double doubleField;
+    private float floatField;
+    private Long id;
+    private int intField;
+    private long longField;
+    private short shortField;
+
+    public boolean getBooleanField() {
+        return booleanField;
+    }
+    public void setBooleanField(boolean booleanField) {
+        this.booleanField = booleanField;
+    }
+    public byte getByteField() {
+        return byteField;
+    }
+    public void setByteField(byte byteField) {
+        this.byteField = byteField;
+    }
+    public char getCharField() {
+        return charField;
+    }
+    public void setCharField(char charField) {
+        this.charField = charField;
+    }
+    public double getDoubleField() {
+        return doubleField;
+    }
+    public void setDoubleField(double doubleField) {
+        this.doubleField = doubleField;
+    }
+    public float getFloatField() {
+        return floatField;
+    }
+    public void setFloatField(float floatField) {
+        this.floatField = floatField;
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public int getIntField() {
+        return intField;
+    }
+    public void setIntField(int intField) {
+        this.intField = intField;
+    }
+    public long getLongField() {
+        return longField;
+    }
+    public void setLongField(long longField) {
+        this.longField = longField;
+    }
+    public short getShortField() {
+        return shortField;
+    }
+    public void setShortField(short shortField) {
+        this.shortField = shortField;
+    }
+}
