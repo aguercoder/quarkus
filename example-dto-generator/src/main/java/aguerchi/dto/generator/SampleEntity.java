67d33c14d313d05f179dafaac1184c93b9531fc9
aguercoder <aguer.coder@gmail.com>
2023-09-23 11:09:09 +0200

    Update dependencie dto generator

--- example-dto-generator/src/main/java/aguerchi/dto/generator/SampleEntity.java
+++ example-dto-generator/src/main/java/aguerchi/dto/generator/SampleEntity.java
@@ -1,99 +1,25 @@
 package aguerchi.dto.generator;
 
-import ag.codegen.quarkus.runtime.annotation.DtoGenerator;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+import java.util.Date;
+
+import com.agcodegen.quarkus.runtime.annotation.DtoGenerator;
+
 import jakarta.persistence.Id;
 
-@Entity
 @DtoGenerator
 public class SampleEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private int intField;
-    private double doubleField;
-    private float floatField;
-    private long longField;
-    private short shortField;
-    private byte byteField;
-    private char charField;
-    private boolean booleanField;
+    private Boolean objectBooleanField;
 
-    // Getter and Setter methods for each field
+    private String stringField;
+    private Date dateField;
 
-    public Long getId() {
-        return id;
-    }
+    // Constructeurs, getters et setters (omis pour plus de concision)
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public int getIntField() {
-        return intField;
-    }
-
-    public void setIntField(int intField) {
-        this.intField = intField;
-    }
-
-    public double getDoubleField() {
-        return doubleField;
-    }
-
-    public void setDoubleField(double doubleField) {
-        this.doubleField = doubleField;
-    }
-
-    public float getFloatField() {
-        return floatField;
-    }
-
-    public void setFloatField(float floatField) {
-        this.floatField = floatField;
-    }
-
-    public long getLongField() {
-        return longField;
-    }
-
-    public void setLongField(long longField) {
-        this.longField = longField;
-    }
-
-    public short getShortField() {
-        return shortField;
-    }
-
-    public void setShortField(short shortField) {
-        this.shortField = shortField;
-    }
-
-    public byte getByteField() {
-        return byteField;
-    }
-
-    public void setByteField(byte byteField) {
-        this.byteField = byteField;
-    }
-
-    public char getCharField() {
-        return charField;
-    }
-
-    public void setCharField(char charField) {
-        this.charField = charField;
-    }
-
-    public boolean isBooleanField() {
-        return booleanField;
-    }
-
-    public void setBooleanField(boolean booleanField) {
-        this.booleanField = booleanField;
+    public SampleEntity() {
+        // Constructeur par défaut
     }
 }
--- example-dto-generator/src/main/java/aguerchi/dto/generator/data/response/SampleEntityResponseDTO.java
+++ example-dto-generator/src/main/java/aguerchi/dto/generator/data/response/SampleEntityResponseDTO.java
@@ -1,69 +1,27 @@
 package aguerchi.dto.generator.data.response;
-import java.lang.Long;
+import java.util.Date;
 
 public class SampleEntityResponseDTO {
-    private boolean booleanField;
-    private byte byteField;
-    private char charField;
-    private double doubleField;
-    private float floatField;
-    private Long id;
-    private int intField;
-    private long longField;
-    private short shortField;
+    private Date dateField;
+    private Boolean objectBooleanField;
+    private String stringField;
 
-    public boolean getBooleanField() {
-        return booleanField;
+    public Date getDateField() {
+        return dateField;
     }
-    public void setBooleanField(boolean booleanField) {
-        this.booleanField = booleanField;
+    public void setDateField(Date dateField) {
+        this.dateField = dateField;
     }
-    public byte getByteField() {
-        return byteField;
+    public Boolean getObjectBooleanField() {
+        return objectBooleanField;
     }
-    public void setByteField(byte byteField) {
-        this.byteField = byteField;
+    public void setObjectBooleanField(Boolean objectBooleanField) {
+        this.objectBooleanField = objectBooleanField;
     }
-    public char getCharField() {
-        return charField;
+    public String getStringField() {
+        return stringField;
     }
-    public void setCharField(char charField) {
-        this.charField = charField;
-    }
-    public double getDoubleField() {
-        return doubleField;
-    }
-    public void setDoubleField(double doubleField) {
-        this.doubleField = doubleField;
-    }
-    public float getFloatField() {
-        return floatField;
-    }
-    public void setFloatField(float floatField) {
-        this.floatField = floatField;
-    }
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public int getIntField() {
-        return intField;
-    }
-    public void setIntField(int intField) {
-        this.intField = intField;
-    }
-    public long getLongField() {
-        return longField;
-    }
-    public void setLongField(long longField) {
-        this.longField = longField;
-    }
-    public short getShortField() {
-        return shortField;
-    }
-    public void setShortField(short shortField) {
-        this.shortField = shortField;
+    public void setStringField(String stringField) {
+        this.stringField = stringField;
     }
 }
